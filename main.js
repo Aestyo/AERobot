fa0f2b959cc0c2ff818eebbff10d79084c66de6b
Mathis Sauvage <sauvagemathis@gmail.com>
2020-03-30 19:35:39 +0200

    Correctifs 3

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 // Amorçage des modules nécessaires
 const { Client } = require("discord.js");
 const client = new Client();
+const config = require("./config.json");
 
 // Évènement : Démarrage du bot terminé
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`INFO: Bot connecté en tant que ${client.user.tag}`);
 });
 
 // Évènement : Récéption d'un message par le bot
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.author.id == config.cookieID) {
+    message.react("🍪");
+  }
+  if (message.content.startsWith(config.prefix)) {
+    const args = message.content.split("/ +g");
+    const cmd = args.shift().toLowerCase;
+
+    if (cmd == "dis") {
+      message.channel.send(args.join(" "));
+      message.channel.delete;
+    }
   }
 });
 
 // Connexion du bot aux serveurs de Discord
-const { token } = require("./config.json");
-client.login(token);
+client.login(config.token);
+
+// Commande complète : args.join(" ");
