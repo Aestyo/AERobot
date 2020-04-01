9120751d308353b99c3100e4a44cd363caa5b45e
Mathis Sauvage <sauvagemathis@gmail.com>
2020-04-01 02:59:08 +0200

    v1.0.6

--- bin/commands/JDR/roll.js
+++ bin/commands/JDR/roll.js
@@ -2,10 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   var total = 0;
-  const commande = args
-    .slice(0)
-    .join(" ")
-    .split("d");
+  const commande = args[0].split("d");
   if (commande[0] > 25)
     return message.channel.send(
       "Vous ne pouvez pas lancer plus de 25 dés à la fois."
@@ -23,9 +20,6 @@ module.exports.run = async (client, message, args) => {
       message.author.avatarURL(),
       "https://roll20.net/"
     )
-    .setDescription(
-      "Lancé de " + commande[0] + " dé(s) de " + commande[1] + " faces."
-    )
     .setThumbnail(
       "https://cdn.discordapp.com/attachments/690260695186800641/694692567517757460/rollingdices.png"
     )
@@ -42,14 +36,35 @@ module.exports.run = async (client, message, args) => {
     roll[i] = Math.floor(Math.random() * commande[1] + 1);
     total = total + roll[i];
     Embed.addFields({
-      name: `Tirage du dé n° ${i + 1}`,
+      name: `Tirage du dé ${i + 1}`,
       value: `${roll[i]} / 100`,
       inline: true
     });
   }
-  Embed.addFields({
-    name: "__**Total des dés jetés**__ :",
-    value: `${total} / ${commande[0] * commande[1]}`
-  });
+  if (args[1] == undefined) {
+    Embed.setDescription(
+      "Lancé de " + commande[0] + " dé(s) de " + commande[1] + " faces."
+    );
+    Embed.addFields({
+      name: "__**Total des dés jetés**__ :",
+      value: `${total} / ${commande[0] * commande[1]}`
+    });
+  } else {
+    const add = args[1].split("+");
+    Embed.setDescription(
+      "Lancé de " +
+        commande[0] +
+        " dé(s) de " +
+        commande[1] +
+        " faces + " +
+        add[1] +
+        "."
+    );
+    Embed.addFields({
+      name: "__**Total des dés jetés**__ :",
+      value: `${total + parseInt(add[1])} / ${commande[0] * commande[1] +
+        parseInt(add[1])}`
+    });
+  }
   message.channel.send(Embed);
 };
