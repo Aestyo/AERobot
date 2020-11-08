b0a5c68a862cee686308ee8aab71387f8be3eb59
Æstyo <sauvagemathis@gmail.com>
2020-11-08 16:59:34 +0100

    2.0.2.0

--- models/reaction.js
+++ models/reaction.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const reactionSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectID,
-  message: Object,
+  message: String,
   reaction: String,
-  result: String,
+  action: String,
+  subject: String,
 });
 
 module.exports = mongoose.model("Reaction", reactionSchema);
