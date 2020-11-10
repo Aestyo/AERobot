a205211757d199fd2627f904331c8740b65764cf
Æstyo <sauvagemathis@gmail.com>
2020-11-10 16:57:13 +0100

    2.0.2.4

--- bin/commands/core/join.js
+++ bin/commands/core/join.js
@@ -14,30 +14,19 @@ function Play(connection, message) {
   });
 }
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (message) => {
   if (message.member.voice.channel) {
     const connection = await message.member.voice.channel.join().then((connection) => {
       console.log(`INFO : Connecté à ${message.member.voice.channel} ${message.member.voice.channel.name}`);
-      if (message.author.id == `293802580918665216` || message.author.id == `670697151235620886`) {
-        message.channel.send(`je Me CoNnEcTe DaNs Le ChAnNeL ${message.member.voice.channel.name}`);
-      } else {
-        message.channel.send(`Je me connecte dans le channel ${message.member.voice.channel.name}`);
-      }
-      connection.play(`D:/DÃ©veloppement/Ã†Robot/data/sounds/connection.mp3`, {
-        volume: 0.5,
-      });
-      if (!servers[message.guild.id]) {
+      connection.play(`../../../data/sounds/connection.mp3`, {volume: 0.5,});
+      /*if (!servers[message.guild.id]) {
         servers[message.guild.id] = { queue: [] };
       }
       server = servers[message.guild.id];
       server.queue.push(args);
-      Play(connection, message);
+      Play(connection, message);*/
     });
   } else {
     message.reply("Tu dois d'abord être dans un channel vocal pour ça.");
   }
-};
-
-module.exports.help = {
-  name: "viens",
 };
--- bin/commands/images/meme.js
+++ bin/commands/images/meme.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const request = require("request");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
   console.log("INFO : Envoie d'une requête à meme-api.com...");
   let debut = Date.now();
   request("https://meme-api.herokuapp.com/gimme", function (err, res, body) {
@@ -10,7 +10,6 @@ module.exports.run = async (bot, message, args) => {
     console.log(
       `INFO : Réponse reçue en ${latence} ms. Réponse reçue : ${body[15]}`
     );
-    message.reply(`Voilà un bon meme pour toi ! `);
     message.channel.send(`${body[15]} Titre : **${body[11]}** de /r/Memes.`);
   });
 };
