082e3aac8a63bee7c0fa8c36c14c056e731db374
unknown <sauvagemathis@gmail.com>
2021-02-02 21:22:20 +0100

    3.2.5

--- bin/commands/admin/test.js
+++ bin/commands/admin/test.js
@@ -14,68 +14,68 @@ module.exports.run = async (client, message, args) => {
     if(minutes != 0){str = str + `${minutes} minutes, `}
     if(seconds != 0){str = str + `${seconds} secondes `}
     try {
-        channel.message.send(os.cpus()[0].model);
+       console.log(os.cpus()[0].model);
     }catch(error){
-        message.channel.send("Erreur! os.cpus()[0].model");
+        console.log("Erreur! os.cpus()[0].model");
     }
     try {
-        channel.message.send(os.arch());
+        console.log(os.arch());
     }catch(error){
-        message.channel.send("Erreur! os.arch()");
+        console.log("Erreur! os.arch()");
     }
     try {
-        channel.message.send(os.cpus()[0].speed/1000);
+        console.log(os.cpus()[0].speed/1000);
     }catch(error){
-        message.channel.send("Erreur! os.cpus()[0].speed/1000");
+        console.log("Erreur! os.cpus()[0].speed/1000");
     }
     try {
-        channel.message.send(os.cpus().length);
+        console.log(os.cpus().length);
     }catch(error){
-        message.channel.send("Erreur! os.cpus().length");
+        console.log("Erreur! os.cpus().length");
     }
     try {
-        channel.message.send(os.hostname());
+        console.log(os.hostname());
     }catch(error){
-        message.channel.send("Erreur! os.hostname()");
+        console.log("Erreur! os.hostname()");
     }
     try {
-        channel.message.send(os.type());
+        console.log(os.type());
     }catch(error){
-        message.channel.send("Erreur! os.type()");
+        console.log("Erreur! os.type()");
     }
     try {
-        channel.message.send(os.release());
+        console.log(os.release());
     }catch(error){
-        message.channel.send("Erreur! os.release()");
+        console.log("Erreur! os.release()");
     }
     try {
-        channel.message.send(os.platform());
+        console.log(os.platform());
     }catch(error){
-        message.channel.send("Erreur! os.platform()");
+        console.log("Erreur! os.platform()");
     }
     try {
-        channel.message.send(str);
+        console.log(str);
     }catch(error){
-        message.channel.send("Erreur! str");
+        console.log("Erreur! str");
     }
     try {
-        channel.message.send(os.totalmem()/1000000000);
+        console.log(os.totalmem()/1000000000);
     }catch(error){
-        message.channel.send("Erreur! (os.totalmem()/1000000000).toFixed(2)");
+        console.log("Erreur! (os.totalmem()/1000000000).toFixed(2)");
     }
     try {
-        channel.message.send(os.freemem()/1000000000);
+        console.log(os.freemem()/1000000000);
     }catch(error){
-        message.channel.send("Erreur! (os.freemem()/1000000000).toFixed(2)");
+        console.log("Erreur! (os.freemem()/1000000000).toFixed(2)");
     }
     try {
-        channel.message.send(Math.round(used * 100) / 100);
+        console.log(Math.round(used * 100) / 100);
     }catch(error){
-        message.channel.send("Erreur! {Math.round(used * 100) / 100");
+        console.log("Erreur! {Math.round(used * 100) / 100");
     }
     try {
-        channel.message.send(os.cpus()[0].model);
+        console.log(os.cpus()[0].model);
     }catch(error){
-        message.channel.send("Erreur! os.cpus()[0].model");
+        console.log("Erreur! os.cpus()[0].model");
     }
 };
